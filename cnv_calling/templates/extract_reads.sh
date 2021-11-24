ec1129832492bd9af2e34f277df1215f4f516ee4
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-11-24 16:03:29 +0100

    Renamed root file to be unique

--- cnv_calling/templates/extract_reads.sh
+++ cnv_calling/templates/extract_reads.sh
@@ -2,4 +2,5 @@
 
 module load ROOT/6.06.08 bioinfo-tools CNVnator
 chrom=$(seq 1 22)
-cnvnator -root root -chrom $chrom -tree !{bam} -unique
+root_file="$(basename !{bam} .bam).root"
+cnvnator -root "$root_file" -chrom $chrom -tree "!{bam}" -unique
