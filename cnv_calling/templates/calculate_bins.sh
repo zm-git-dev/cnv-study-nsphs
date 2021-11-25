ff3c44a05438cff39da4c7e457c3c2b318e921a5
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-11-25 13:08:31 +0100

    Calculate_bins sems to work now

--- cnv_calling/templates/calculate_bins.sh
+++ cnv_calling/templates/calculate_bins.sh
@@ -5,19 +5,20 @@ module load ROOT/6.06.08 bioinfo-tools CNVnator
 bin_sizes=(70 85 100 150 200 250)
 chrom=$(seq 1 22)
 
-max_ratio=5
-best_size='-1'
 for bin_size in $bin_sizes
 do
   cnvnator -root "!{root}" -his "$bin_size" -chrom $chrom -d "!{reference}"
   # CNVnator outputs a log message from which we have to extract the mean RD and its SD.
-  bin_stats=($(cnvnator -root "!{root}" -stat "$bin_size" -chrom $chrom | perl -ne 'print "$1\t$2\n" if /^Average RD per bin \(1-22\) is (\d+\.\d+) \+- (\d+\.\d+) \(after GC correction\)/'))
-  let "ratio=${bin_stats[1]}/${bin_stats[2]}"
-  # The optimum ratio is between 4 and 5. The smaller, the better.
-  # Therefore, the smallest found ratio between 4 and 5 is the upper bound for the optimum
-  if [[ "$ratio" -le "$max_ratio" && "$ratio" -ge 4 ]]; then
-    max_ratio=$ratio
-    best_size=$bin_size
-  fi
+  echo "$(cnvnator -root "!{root}" -stat "$bin_size" -chrom $chrom | perl -ne 'print "$1 $2\n" if /^Average RD per bin \(1-22\) is (\d+\.\d+) \+- (\d+\.\d+) \(after GC correction\)/') $bin_size" >> stats.txt
 done
-echo $best_size
+
+max_ratio=5
+best_size=-1
+while read -r mean sd size; do
+  ratio=$(echo "$mean / $sd" | bc -l)
+  echo $ratio $size
+  if [[ $ratio -le $max_ratio && $ratio -ge 4 ]]; then
+    max_ratio=$ratio
+    best_size=$size
+  fi
+done < stats.txt
