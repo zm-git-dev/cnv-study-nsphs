32ccc56d71ab6a2cda50bd110c5da868f5eba0e2
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-11-26 10:36:37 +0100

    Bin size calculation works now

--- cnv_calling/templates/calculate_bins.sh
+++ cnv_calling/templates/calculate_bins.sh
@@ -7,18 +7,21 @@ chrom=$(seq 1 22)
 
 for bin_size in $bin_sizes
 do
-  cnvnator -root "!{root}" -his "$bin_size" -chrom $chrom -d "!{reference}"
+  cnvnator -root "!{root}" -his "$bin_size" -chrom $chrom -d "!{reference}" > &2
   # CNVnator outputs a log message from which we have to extract the mean RD and its SD.
   echo "$(cnvnator -root "!{root}" -stat "$bin_size" -chrom $chrom | perl -ne 'print "$1 $2\n" if /^Average RD per bin \(1-22\) is (\d+\.\d+) \+- (\d+\.\d+) \(after GC correction\)/') $bin_size" >> stats.txt
 done
 
-max_ratio=5
-best_size=-1
-while read -r mean sd size; do
-  ratio=$(echo "$mean / $sd" | bc -l)
-  echo $ratio $size
-  if [[ $ratio -le $max_ratio && $ratio -ge 4 ]]; then
-    max_ratio=$ratio
-    best_size=$size
-  fi
-done < stats.txt
+awk '
+  BEGIN {
+    max_ratio=5;
+    best_size=-1;
+  }
+  }
+    ratio = $1 / $2;
+    if (ratio >= 4 && ratio <= max_ratio) {
+      best_size=$3;
+      max_ratio=ratio;
+    }
+  }
+  END {print best_size}' stats.txt
