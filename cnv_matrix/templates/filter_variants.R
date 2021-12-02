787e091d1f69f3ddfa491bf307dc5fb530c88133
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-12-02 12:41:47 +0100

    Made bed files compliant with comment in header

--- cnv_matrix/templates/filter_variants.R
+++ cnv_matrix/templates/filter_variants.R
@@ -14,7 +14,7 @@ if (nrow(variants) == 0){ # If the original is empty, the result is empty, too
 names(variants) <- c("type", "coordinates", "size", "rd_norm", "e1", "e2", "e3", "e4", "q0")
 passing_qc <- variants[q0 >= 0][q0 <= 0.5]
 coordinates <- str_split_fixed(passing_qc$coordinates, ":|-", n=3) %>% as.data.table()
-names(coordinates) <- c("chr", "start", "stop")
+names(coordinates) <- c("#chr", "start", "stop")
 result <- cbind(coordinates, passing_qc[, .(cn = rd_norm * 2, e1 = e1, e2 = e2, q0 = q0, size = size)])
 
 write.table(result, results_file, sep="\t", quote=FALSE, row.names=FALSE)
