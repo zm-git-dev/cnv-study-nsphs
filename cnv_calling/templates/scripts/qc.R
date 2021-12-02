7979fd1277e83beb86a6e9cc964482648da40fb1
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-12-02 10:44:26 +0100

    Moved qc.R

--- cnv_calling/templates/scripts/qc.R
+++ cnv_calling/templates/scripts/qc.R
@@ -3,15 +3,14 @@ library(data.table)
 library(stringr)
 library(magrittr)
 
-args <- commandArgs(TRUE)
-variant_file <- args[1]
+variant_file <- "!{variants}"
 
 variants <- fread(variant_file)
-names(variants) <- c("type", "coordinates", "size", "rd_norm", "e1", "e2", "e3". "e4", "q0")
+names(variants) <- c("type", "coordinates", "size", "rd_norm", "e1", "e2", "e3", "e4", "q0")
 passing_qc <- variants[q0 >= 0][q0 <= 0.5][size >= 1000][e1 <= 0.00005]
 coordinates <- str_split_fixed(passing_qc$coordinates, ":|-", n=3) %>% as.data.table()
 names(coordinates) <- c("chr", "start", "stop")
 result <- cbind(coordinates, passing_qc[, .(cn = rd_norm * 2, e1 = e1, e2 = e2, q0 = q0, size = size)])
 
-results_file <- basename($variants) %>% str_replace(".txt", "_qc.bed")
+results_file <- basename(variant_file) %>% str_replace(".txt", "_qc.bed")
 write.table(result, results_file, sep="\t", quote=FALSE, row.names=FALSE)
