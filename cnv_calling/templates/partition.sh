3b739d6281ba98186f2987fa8ed682c36ccdc508
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-12-02 10:45:30 +0100

    Paritioning does not need to strip newlines anymore

--- cnv_calling/templates/partition.sh
+++ cnv_calling/templates/partition.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 module load ROOT/6.06.08 bioinfo-tools CNVnator
 chrom=$(seq 1 22)
-bin_size=$(echo "!{bin_size}" | xargs)
-cnvnator -root !{root} -partition "${bin_size}" -chrom $chrom
+cnvnator -root !{root} -partition "!{bin_size}" -chrom $chrom
